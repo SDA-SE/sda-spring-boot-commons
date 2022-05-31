e73dbca0e1d19510fd79b3a244f13193fe928437
Jesko Steinberg <jesko.steinberg@sda.se>
2022-05-31 11:20:17 +0200

    fix(mongodb-starter): ensure index creation programmatically, since it's not recommended to enable auto index creation
    https://github.com/spring-projects/spring-data-mongodb/issues/3049

--- sda-spring-boot-commons-web-autoconfigure/src/main/java/org/sdase/commons/spring/boot/web/error/ApiException.java
+++ sda-spring-boot-commons-web-autoconfigure/src/main/java/org/sdase/commons/spring/boot/web/error/ApiException.java
@@ -1,3 +1,8 @@
+/*
+* Copyright (c). SDA SE Open Industry Solutions (https://www.sda.se).
+*
+* All rights reserved.
+*/
 package org.sdase.commons.spring.boot.web.error;
 
 import java.util.ArrayList;
