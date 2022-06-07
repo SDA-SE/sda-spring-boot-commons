cf087a189517a006dd6bdfc3abb62f3a6ae3c0a7
Jesko Steinberg <jesko.steinberg@sda.se>
2022-06-07 17:01:51 +0200

    fix(web): add ApiException to spring context

---
+++ sda-spring-boot-commons-web-autoconfigure/src/test/java/org/sdase/commons/spring/boot/web/error/ErrorController.java
@@ -0,0 +1,25 @@
+package org.sdase.commons.spring.boot.web.error;
+
+import org.sdase.commons.spring.boot.web.error.ApiException.FinalBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ErrorController {
+  @PostMapping("/throw")
+  public ResponseEntity<?> throwApiException(@RequestBody ApiError apiError) {
+    ApiInvalidParam apiInvalidParam =
+        apiError.getInvalidParams().isEmpty() ? null : apiError.getInvalidParams().get(0);
+    FinalBuilder builder =
+        ApiException.builder()
+            .httpCode(Integer.parseInt(apiError.getTitle()))
+            .title(apiError.getTitle());
+    if (apiInvalidParam != null) {
+      builder.detail(
+          apiInvalidParam.getField(), apiInvalidParam.getReason(), apiInvalidParam.getErrorCode());
+    }
+    throw builder.build();
+  }
+}
