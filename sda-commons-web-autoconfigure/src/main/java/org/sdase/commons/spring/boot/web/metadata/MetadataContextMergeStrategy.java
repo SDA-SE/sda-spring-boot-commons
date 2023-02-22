3de92520e5e09e3ec99724ed6586625b5b9d67da
Eike Nils Knopp <eike.knopp@sda.se>
2023-02-22 15:51:03 +0100

    feat: add basic metadata context

---
+++ sda-commons-web-autoconfigure/src/main/java/org/sdase/commons/spring/boot/web/metadata/MetadataContextMergeStrategy.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2022- SDA SE Open Industry Solutions (https://www.sda.se)
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+package org.sdase.commons.spring.boot.web.metadata;
+
+/**
+ * Strategies for merging data from a {@link DetachedMetadataContext} into the {@linkplain
+ * MetadataContext#current() current metadata context}.
+ */
+public enum MetadataContextMergeStrategy {
+
+  /** All values for a specific key will be merged into a distinct list of values. */
+  EXTEND,
+
+  /**
+   * New values take precedence. If new values for a specific key exist, already existing values
+   * will be replaced by the new values. If no new values exist, old values are kept.
+   */
+  REPLACE,
+
+  /**
+   * Existing values take precedence. If values for a specific key exist in the current context,
+   * they are kept and new values for this key are ignored. If no values for a specific key exist in
+   * the current context, the new values are used.
+   */
+  KEEP
+}
---
+++ sda-commons-web-autoconfigure/src/main/java/org/sdase/commons/spring/boot/web/metadata/MetadataContextUtil.java
@@ -0,0 +1,132 @@
+/*
+ * Copyright 2022- SDA SE Open Industry Solutions (https://www.sda.se)
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+package org.sdase.commons.spring.boot.web.metadata;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.concurrent.Callable;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.apache.commons.lang3.StringUtils;
+
+class MetadataContextUtil {
+
+  private MetadataContextUtil() {
+    // no instance needed
+  }
+
+  static MetadataContext current() {
+    return MetadataContextHolder.get();
+  }
+
+  static Runnable transferMetadataContext(Runnable runnable) {
+    MetadataContext metadataContext = MetadataContextHolder.get();
+
+    return () -> {
+      try {
+        MetadataContextHolder.set(metadataContext);
+        runnable.run();
+      } finally {
+        MetadataContextHolder.clear();
+      }
+    };
+  }
+
+  static <V> Callable<V> transferMetadataContext(Callable<V> callable) {
+    MetadataContext metadataContext = MetadataContextHolder.get();
+
+    return () -> {
+      try {
+        MetadataContextHolder.set(metadataContext);
+        return callable.call();
+      } finally {
+        MetadataContextHolder.clear();
+      }
+    };
+  }
+
+  static String keyFromConfiguration(String environmentOrPropertyName)
+      throws KeyConfigurationMissingException {
+    return determineConfiguredValue(environmentOrPropertyName)
+        .orElseThrow(() -> new KeyConfigurationMissingException(environmentOrPropertyName));
+  }
+
+  static Set<String> metadataFields() {
+    return determineConfiguredValue(MetadataContext.METADATA_FIELDS_ENVIRONMENT_VARIABLE)
+        .map(MetadataContextUtil::toSet)
+        .orElse(Set.of());
+  }
+
+  static DetachedMetadataContext merge(
+      DetachedMetadataContext context1, DetachedMetadataContext context2) {
+    Set<String> keys =
+        Stream.concat(context1.keySet().stream(), context2.keySet().stream())
+            .collect(Collectors.toSet());
+    var result = new DetachedMetadataContext();
+    for (String key : keys) {
+      var normalizedValues1 = normalizeValues(context1.get(key));
+      var normalizedValues2 = normalizeValues(context2.get(key));
+      var newValues =
+          Stream.concat(normalizedValues1, normalizedValues2)
+              .distinct()
+              .collect(Collectors.toList());
+      result.put(key, newValues);
+    }
+    return result;
+  }
+
+  static DetachedMetadataContext mergeWithPreference(
+      DetachedMetadataContext preferredContextData, DetachedMetadataContext secondaryContextData) {
+    Set<String> keys =
+        Stream.concat(
+                preferredContextData.keySet().stream(), secondaryContextData.keySet().stream())
+            .collect(Collectors.toSet());
+    var result = new DetachedMetadataContext();
+    for (String key : keys) {
+      var preferredValues =
+          normalizeValues(preferredContextData.get(key)).collect(Collectors.toList());
+      if (!preferredValues.isEmpty()) {
+        result.put(key, preferredValues);
+      } else {
+        var secondaryValues =
+            normalizeValues(secondaryContextData.get(key)).collect(Collectors.toList());
+        result.put(key, secondaryValues);
+      }
+    }
+    return result;
+  }
+
+  private static Stream<String> normalizeValues(List<String> values) {
+    if (values == null) {
+      return Stream.of();
+    }
+    return values.stream().filter(StringUtils::isNotBlank);
+  }
+
+  private static Set<String> toSet(String commaDelimitedValues) {
+    var values = Arrays.asList(commaDelimitedValues.split(","));
+    return values.stream()
+        .filter(StringUtils::isNotBlank)
+        .map(String::trim)
+        .collect(Collectors.toSet());
+  }
+
+  private static Optional<String> determineConfiguredValue(String environmentOrPropertyName) {
+    var fromProperty = System.getProperty(environmentOrPropertyName);
+    if (StringUtils.isNotBlank(fromProperty)) {
+      return Optional.of(fromProperty);
+    }
+    String fromEnvironment = System.getenv(environmentOrPropertyName);
+    if (StringUtils.isNotBlank(fromEnvironment)) {
+      return Optional.of(fromEnvironment);
+    }
+    return Optional.empty();
+  }
+}
