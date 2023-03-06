50ced6b9381cef3abbb47a621e8b7a9dcc91fc35
Jörg Siebahn <joerg.siebahn@sda-se.com>
2023-03-06 11:54:41 +0100

    test(validation): make test independent of local language settings

--- sda-commons-web-autoconfigure/src/test/java/org/sdase/commons/spring/boot/web/security/test/SecurityTestApp.java
+++ sda-commons-web-autoconfigure/src/test/java/org/sdase/commons/spring/boot/web/security/test/SecurityTestApp.java
@@ -17,7 +17,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @SpringBootApplication
@@ -98,7 +104,7 @@ public class SecurityTestApp {
 
   @PostMapping(value = "/validate")
   public ResponseEntity<String> validateTestResource(
-      @Valid @NumericString @RequestBody TestResource testResource) {
+      @Valid @RequestBody TestResource testResource) {
     LOGGER.info(
         "Received valid value {} and postcode {} ",
         testResource.getValue(),
