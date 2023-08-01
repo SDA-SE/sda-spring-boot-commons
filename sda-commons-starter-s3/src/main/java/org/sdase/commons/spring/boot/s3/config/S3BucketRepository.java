f76956ef689494b09a8c26f4d6a7ca602dc20bed
Jörg Siebahn <joerg.siebahn@sda-se.com>
2023-08-01 16:16:48 +0200

    fix(s3): temporary quick fix for S3 compile errors

--- sda-commons-starter-s3/src/main/java/org/sdase/commons/spring/boot/s3/config/S3BucketRepository.java
+++ sda-commons-starter-s3/src/main/java/org/sdase/commons/spring/boot/s3/config/S3BucketRepository.java
@@ -7,26 +7,29 @@
  */
 package org.sdase.commons.spring.boot.s3.config;
 
-import com.amazonaws.AmazonServiceException;
-import com.amazonaws.SdkClientException;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.*;
-import com.amazonaws.util.IOUtils;
+import io.awspring.cloud.s3.S3Exception;
+import io.awspring.cloud.s3.S3Operations;
+import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import software.amazon.awssdk.core.exception.SdkClientException;
+import software.amazon.awssdk.core.exception.SdkException;
+import software.amazon.awssdk.utils.IoUtils;
 
 public class S3BucketRepository {
 
   private static final Logger LOG = LoggerFactory.getLogger(S3BucketRepository.class);
   private final String bucketName;
-  private final AmazonS3 amazonS3;
+  private final S3Operations s3Operations;
 
-  public S3BucketRepository(AmazonS3 amazonS3, String bucketName) {
-    this.amazonS3 = amazonS3;
+  public S3BucketRepository(S3Operations s3Operations, String bucketName) {
+    this.s3Operations = s3Operations;
     this.bucketName = bucketName;
   }
 
@@ -35,17 +38,13 @@ public class S3BucketRepository {
    *
    * @param key The key under which the desired object is stored.
    * @return the content of the object as byte array in the S3 bucket with the given key.
-   * @throws SdkClientException If any errors are encountered in the s3 client while making the
-   *     request or handling the response.
-   * @throws AmazonServiceException If any errors occurred in Amazon S3 while processing the
-   *     request.
-   * @throws IOException if the object content could not be provided as byte array.
+   * @throws FileNotFoundException – if the underlying resource does not exist
+   * @throws IOException – if the content stream could not be opened
    */
   public byte[] findByName(String key) throws IOException {
-    try (var file = amazonS3.getObject(bucketName, key)) {
-      var s3ObjectInputStream = file.getObjectContent();
-
-      return IOUtils.toByteArray(s3ObjectInputStream);
+    var resource = s3Operations.download(bucketName, key);
+    try (var is = resource.getInputStream()) {
+      return IoUtils.toByteArray(is);
     }
   }
 
@@ -55,23 +54,20 @@ public class S3BucketRepository {
    *
    * @param key The key under which to store the specified file
    * @param file The file containing the data to be uploaded to Amazon S3.
-   * @throws SdkClientException If any errors are encountered in the s3 client while making the
-   *     request or handling the response.
-   * @throws AmazonServiceException If any errors occurred in Amazon S3 while processing the
-   *     request.
-   * @throws IOException if the passed file does not exist nor is a file to upload.
+   * @throws FileNotFoundException if the file does not exist, is a directory rather than a regular
+   *     file, or for some other reason cannot be opened for reading.
+   * @throws SecurityException if a security manager exists and its checkRead method denies read
+   *     access to the file.
+   * @throws S3Exception if an error occurs on upload
+   * @throws IOException if the given file can't be closed after reading
    */
-  public void saveFile(String key, File file) throws IOException {
-    if (!file.exists()) {
-      throw new FileNotFoundException(
-          String.format("No file exists in the provided path %s ", file));
-    }
-    if (!file.isFile()) {
-      throw new IOException(String.format("The path '%s' is not a file)", file.getAbsolutePath()));
-    }
+  public void saveFile(String key, File file) throws S3Exception, IOException, SecurityException {
     LOG.info("Uploading file with name {}...", file.getName());
-    amazonS3.putObject(bucketName, key, file);
-    LOG.info("Done! File with name '{}' has been uploaded to the '{}'", file.getName(), bucketName);
+    try (var is = new FileInputStream(file)) {
+      s3Operations.upload(bucketName, key, is);
+      LOG.info(
+          "Done! File with name '{}' has been uploaded to the '{}'", file.getName(), bucketName);
+    }
   }
 
   /**
@@ -81,30 +77,22 @@ public class S3BucketRepository {
    * @param key The key under which to store the specified content
    * @param content The data to be uploaded to Amazon S3. Will be saved as UTF-8.
    * @throws IOException If the content is null
-   * @throws SdkClientException If any errors are encountered in the s3 client while making the
-   *     request or handling the response.
-   * @throws AmazonServiceException If any errors occurred in Amazon S3 while processing the
-   *     request.
+   * @throws S3Exception if an error occurs on upload
    */
   public void save(String key, String content) throws IOException {
     if (content == null) {
       throw new IOException(String.format("Content to be saved by key %s must not be null!", key));
     }
-
     LOG.info("Uploading data with key {}...", key);
-    amazonS3.putObject(bucketName, key, content);
-    LOG.info("Done! Data with key '{}' has been uploaded to the '{}'", key, bucketName);
+    try (var is = new ByteArrayInputStream(content.getBytes(StandardCharsets.UTF_8))) {
+      s3Operations.upload(bucketName, key, is);
+      LOG.info("Done! Data with key '{}' has been uploaded to the '{}'", key, bucketName);
+    }
   }
 
-  /**
-   * This method provides a list of all the objects stored in the s3 bucket. The {@linkplain
-   * S3ObjectSummary#getKey() keys} are resolved by the {@linkplain S3ObjectSummary}.
-   *
-   * @return the list of keys of every file in the bucket.
-   */
+  // TODO needs to be deprecated in current release and then removed in v3
   public List<String> listings() {
-    ObjectListing listing = amazonS3.listObjects(bucketName);
-    return listing.getObjectSummaries().stream().map(S3ObjectSummary::getKey).toList();
+    throw new UnsupportedOperationException("This method will not be provided any more.");
   }
 
   /**
@@ -112,14 +100,16 @@ public class S3BucketRepository {
    * the object does not exist.
    *
    * @param key the key of the file to be deleted in s3 bucket
-   * @throws SdkClientException If any errors are encountered in the s3 client while making the
-   *     request or handling the response.
-   * @throws AmazonServiceException If any errors occurred in Amazon S3 while processing the
-   *     request.
+   * @throws SdkException Base class for all exceptions that can be thrown by the SDK (both service
+   *     and client). Can be used for catch all scenarios.
+   * @throws SdkClientException If any client side error occurs such as an IO related failure,
+   *     failure to get credentials, etc.
+   * @throws S3Exception Base class for all service exceptions. Unknown exceptions will be thrown as
+   *     an instance of this type.
    */
   public void deleteFile(String key) {
     LOG.info("Deleting file with key='{}' ...", key);
-    amazonS3.deleteObject(bucketName, key);
+    s3Operations.deleteObject(bucketName, key);
     LOG.info("Done! File with key='{}' deleted successfully!", key);
   }
 
@@ -128,12 +118,8 @@ public class S3BucketRepository {
    *
    * @param key the key of the object
    * @return true, if object exist.
-   * @throws AmazonServiceException If any errors occurred in Amazon S3 while processing the
-   *     request.
-   * @throws SdkClientException If any errors are encountered in the s3 client while making the
-   *     request or handling the response.
    */
   public boolean doesObjectExist(String key) {
-    return amazonS3.doesObjectExist(bucketName, key);
+    return s3Operations.download(bucketName, key).exists();
   }
 }
