beec0b6a1ec1c062a63d54e8ff67d133ebd6a981
Eike Nils Knopp <eike.nils.knopp@gmail.com>
2023-08-03 11:21:11 +0200

    feat: migrate spring security

---
+++ sda-commons-web-autoconfigure/src/main/java/org/sdase/commons/spring/boot/web/auth/SdaAuthorizationManager.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2022- SDA SE Open Industry Solutions (https://www.sda.se)
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+package org.sdase.commons.spring.boot.web.auth;
+
+import java.util.function.Supplier;
+import org.sdase.commons.spring.boot.web.auth.management.ManagementAuthorizationManager;
+import org.sdase.commons.spring.boot.web.auth.opa.OpaAuthorizationManager;
+import org.sdase.commons.spring.boot.web.auth.opa.OpaExcludesAuthorizationManager;
+import org.springframework.security.authorization.AuthorizationDecision;
+import org.springframework.security.authorization.AuthorizationManager;
+import org.springframework.security.authorization.AuthorizationManagers;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SdaAuthorizationManager implements AuthorizationManager<RequestAuthorizationContext> {
+  private final AuthorizationManager<RequestAuthorizationContext> authorizationManager;
+
+  public SdaAuthorizationManager(
+      ManagementAuthorizationManager managementAccessDecisionVoter,
+      OpaExcludesAuthorizationManager opaExcludesDecisionVoter,
+      OpaAuthorizationManager opaAccessDecisionVoter) {
+    authorizationManager =
+        AuthorizationManagers.anyOf(
+            managementAccessDecisionVoter, opaExcludesDecisionVoter, opaAccessDecisionVoter);
+  }
+
+  @Override
+  public void verify(
+      Supplier<Authentication> authentication, RequestAuthorizationContext filterInvocation) {
+    authorizationManager.verify(authentication, filterInvocation);
+  }
+
+  @Override
+  public AuthorizationDecision check(
+      Supplier<Authentication> authentication, RequestAuthorizationContext filterInvocation) {
+    return authorizationManager.check(authentication, filterInvocation);
+  }
+}
