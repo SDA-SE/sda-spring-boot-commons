d88a6e744c4356d3fc48c293d11105a4ecd02455
Jörg Siebahn <joerg.siebahn@sda-se.com>
2023-08-18 17:06:54 +0200

    docs(cloudevents): add CloudEvents to documentation

--- sda-commons-cloudevents/src/test/java/org/sdase/commons/spring/boot/cloudevents/app/partner/PartnerCreatedEvent.java
+++ sda-commons-cloudevents/src/test/java/org/sdase/commons/spring/boot/cloudevents/app/partner/PartnerCreatedEvent.java
@@ -7,36 +7,27 @@
  */
 package org.sdase.commons.spring.boot.cloudevents.app.partner;
 
-import com.fasterxml.jackson.annotation.JsonPropertyDescription;
+import com.fasterxml.jackson.annotation.JsonClassDescription;
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.net.URI;
 import org.sdase.commons.spring.boot.cloudevents.CloudEventV1;
 
+@JsonClassDescription("An event that is published when a partner has been created.")
 public class PartnerCreatedEvent extends CloudEventV1<PartnerCreatedEvent.PartnerCreated> {
 
-  @Schema(defaultValue = "`/SDA-SE/partner/partner-stack/partner-service`")
-  @Override
-  public URI getSource() {
-    return super.getSource();
+  private static final String DEFAULT_SOURCE = "/SDA-SE/partner/partner-stack/partner-service";
+  private static final String DEFAULT_TYPE = "com.sdase.partner.ods.partner.created";
+
+  public PartnerCreatedEvent() {
+    super();
+    super.setSource(URI.create(DEFAULT_SOURCE));
+    super.setType(DEFAULT_TYPE);
   }
 
-  @JsonPropertyDescription("`com.sdase.partner.ods.partner.created`")
-  @Schema(defaultValue = "com.sdase.partner.ods.partner.created")
-  @Override
-  public String getType() {
-    return super.getType();
-  }
-
-  public static class PartnerCreated {
-    private String id;
-
-    public String getId() {
-      return id;
-    }
-
-    public PartnerCreated setId(String id) {
-      this.id = id;
-      return this;
-    }
-  }
+  @JsonClassDescription("Details about the created partner.")
+  public record PartnerCreated(
+      @Schema(
+              description = "The unique id of a partner.",
+              example = "FF427BC8-B38F-43CC-8AAB-512843808A18")
+          String id) {}
 }
