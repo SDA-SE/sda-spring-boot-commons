133fdbbeb71dac302f2bbdc7757c93c6ab2b9c75
Jörg Siebahn <joerg.siebahn@sda-se.com>
2023-08-21 11:20:00 +0200

    fix(deps): migrate Feign to Jakarta

--- sda-commons-starter-web/src/test/java/org/sdase/commons/spring/boot/web/monitoring/testing/TracedFeignClient.java
+++ sda-commons-starter-web/src/test/java/org/sdase/commons/spring/boot/web/monitoring/testing/TracedFeignClient.java
@@ -7,18 +7,18 @@
  */
 package org.sdase.commons.spring.boot.web.monitoring.testing;
 
-import static javax.ws.rs.core.MediaType.TEXT_PLAIN;
+import static jakarta.ws.rs.core.MediaType.TEXT_PLAIN;
 
-import feign.jaxrs2.JAXRS2Contract;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import feign.jaxrs.JakartaContract;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
 import org.springframework.cloud.openfeign.FeignClient;
 
 @FeignClient(
     value = "testClient",
     url = "${test.tracing.client.base.url}",
-    configuration = JAXRS2Contract.class)
+    configuration = JakartaContract.class)
 public interface TracedFeignClient {
 
   @GET
