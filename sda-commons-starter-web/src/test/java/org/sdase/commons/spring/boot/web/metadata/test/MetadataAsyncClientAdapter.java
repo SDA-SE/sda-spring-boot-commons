ba5076278ee679d2d9532032ad9a11d3e87e7e7a
Frederic Hübner <frederic.huebner@sda.se>
2023-08-25 12:22:37 +0200

    feat(web): support MetadataContext in PlatformClient

--- sda-commons-starter-web/src/test/java/org/sdase/commons/spring/boot/web/metadata/test/MetadataAsyncClientAdapter.java
+++ sda-commons-starter-web/src/test/java/org/sdase/commons/spring/boot/web/metadata/test/MetadataAsyncClientAdapter.java
@@ -12,7 +12,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import java.util.concurrent.CompletableFuture;
 import org.springframework.scheduling.annotation.Async;
-import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -22,11 +21,15 @@ public class MetadataAsyncClientAdapter {
 
   private final MetadataOtherServiceAuthenticatedClient otherServiceAuthenticatedClient;
 
+  private final MetadataPlatformServiceClient metadataPlatformServiceClient;
+
   public MetadataAsyncClientAdapter(
       ObjectMapper objectMapper,
-      MetadataOtherServiceAuthenticatedClient otherServiceAuthenticatedClient) {
+      MetadataOtherServiceAuthenticatedClient otherServiceAuthenticatedClient,
+      MetadataPlatformServiceClient metadataPlatformServiceClient) {
     this.objectMapper = objectMapper;
     this.otherServiceAuthenticatedClient = otherServiceAuthenticatedClient;
+    this.metadataPlatformServiceClient = metadataPlatformServiceClient;
   }
 
   @Async
@@ -36,6 +39,16 @@ public class MetadataAsyncClientAdapter {
     var time = System.nanoTime();
     var timeAsJsonNode = objectMapper.convertValue(time, JsonNode.class);
     jsonNodes.set("time", timeAsJsonNode);
-    return new AsyncResult<>(jsonNodes).completable();
+    return CompletableFuture.completedFuture(jsonNodes);
+  }
+
+  @Async
+  public CompletableFuture<ObjectNode> getPlatformSomethingAsync() {
+    var something = metadataPlatformServiceClient.getSomething();
+    var jsonNodes = objectMapper.convertValue(something, ObjectNode.class);
+    var time = System.nanoTime();
+    var timeAsJsonNode = objectMapper.convertValue(time, JsonNode.class);
+    jsonNodes.set("time", timeAsJsonNode);
+    return CompletableFuture.completedFuture(jsonNodes);
   }
 }
