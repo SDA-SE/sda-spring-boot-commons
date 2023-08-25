963226f012b7153eb5c1e64114636b37282665e2
Frederic Hübner <frederic.huebner@sda.se>
2023-08-25 12:22:37 +0200

    feat(web): support MetadataContext in PlatformClient

--- sda-commons-starter-web/src/main/java/org/sdase/commons/spring/boot/web/client/PlatformClient.java
+++ sda-commons-starter-web/src/main/java/org/sdase/commons/spring/boot/web/client/PlatformClient.java
@@ -11,6 +11,7 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import org.sdase.commons.spring.boot.web.metadata.MetadataContextClientConfiguration;
 import org.sdase.commons.spring.boot.web.tracing.SdaTraceTokenClientConfiguration;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.core.annotation.AliasFor;
@@ -21,7 +22,8 @@ import org.springframework.core.annotation.AliasFor;
     configuration = {
       SdaTraceTokenClientConfiguration.class,
       OidcClientRequestConfiguration.class,
-      AuthenticationPassThroughClientConfiguration.class
+      AuthenticationPassThroughClientConfiguration.class,
+      MetadataContextClientConfiguration.class
     })
 public @interface PlatformClient {
 
