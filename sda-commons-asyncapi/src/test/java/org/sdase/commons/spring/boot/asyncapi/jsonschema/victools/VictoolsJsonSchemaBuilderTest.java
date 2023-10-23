18157c2d74c69072d785b36dbb65951dd489b36d
Jörg Siebahn <joerg.siebahn@sda-se.com>
2023-10-23 10:15:25 +0200

    feat(asyncapi): describe URI as `format: uri` and `@NotBlank` with `minLength` and `pattern`

--- sda-commons-asyncapi/src/test/java/org/sdase/commons/spring/boot/asyncapi/jsonschema/victools/VictoolsJsonSchemaBuilderTest.java
+++ sda-commons-asyncapi/src/test/java/org/sdase/commons/spring/boot/asyncapi/jsonschema/victools/VictoolsJsonSchemaBuilderTest.java
@@ -9,7 +9,6 @@ package org.sdase.commons.spring.boot.asyncapi.jsonschema.victools;
 
 import java.util.Set;
 import org.sdase.commons.spring.boot.asyncapi.jsonschema.AbstractJsonSchemaBuilderTest;
-import org.sdase.commons.spring.boot.asyncapi.test.data.models.MinimalTestModels.Required.JakartaNotBlank;
 import org.sdase.commons.spring.boot.asyncapi.test.data.models.MinimalTestModels.Required.SwaggerRequiredMode;
 
 class VictoolsJsonSchemaBuilderTest extends AbstractJsonSchemaBuilderTest {
@@ -22,9 +21,6 @@ class VictoolsJsonSchemaBuilderTest extends AbstractJsonSchemaBuilderTest {
   protected Set<DisabledSpec> disableSpecificFieldTests() {
     return Set.of(
         // Schema.requiredMode() not supported, but Schema.required() is
-        disable(SwaggerRequiredMode.class, "/required"),
-        // pattern not set, schema would allow " ", but validation does not
-        // work around: @Pattern(regexp = "^.*\\S+.*$") @NotBlank
-        disable(JakartaNotBlank.class, "/properties/notBlankProperty/pattern"));
+        disable(SwaggerRequiredMode.class, "/required"));
   }
 }
