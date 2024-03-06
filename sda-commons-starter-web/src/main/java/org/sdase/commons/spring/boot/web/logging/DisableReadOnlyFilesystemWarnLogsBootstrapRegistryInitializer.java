2286fa15a273b06d1003ea12e718d4e1e6de4813
Preethi Venkatesh <75310484+preethivenkatesh-sdase@users.noreply.github.com>
2024-03-06 10:32:08 +0100

    fix(starter-web): disable warning logs related to read only temp directories (can't be changed in services)
    * fix(web): add initializer to avoid warn logs for read only filesystems
    
    ---------
    
    Co-authored-by: Jörg Siebahn <joerg.siebahn@sda-se.com>

---
+++ sda-commons-starter-web/src/main/java/org/sdase/commons/spring/boot/web/logging/DisableReadOnlyFilesystemWarnLogsBootstrapRegistryInitializer.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2022- SDA SE Open Industry Solutions (https://www.sda.se)
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+package org.sdase.commons.spring.boot.web.logging;
+
+import org.springframework.boot.BootstrapRegistry;
+import org.springframework.boot.BootstrapRegistryInitializer;
+
+/**
+ * Disables warn logs that will raise if the filesystem is read only.
+ *
+ * @see <a href="https://sda-se.github.io/sda-spring-boot-commons/#static-directories">Docs</a>
+ */
+public class DisableReadOnlyFilesystemWarnLogsBootstrapRegistryInitializer
+    implements BootstrapRegistryInitializer {
+  @Override
+  public void initialize(BootstrapRegistry registry) {
+    System.setProperty("logging.level.org.apache.catalina.core", "error");
+  }
+}
