a8ab839d9549323200dd7494a63577d16dcbb76d
Eike Nils Knopp <eike.nils.knopp@gmail.com>
2024-03-27 16:07:55 +0100

    feat(docs): add documentation for actuator endpoints

---
+++ sda-commons-app-example/src/main/java/org/sdase/commons/spring/boot/web/app/example/AvailableEndpointsActuator.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2022- SDA SE Open Industry Solutions (https://www.sda.se)
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+package org.sdase.commons.spring.boot.web.app.example;
+
+import java.util.List;
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.stereotype.Component;
+
+@Component
+@Endpoint(id = "availableEndpoints")
+public class AvailableEndpointsActuator {
+
+  @ReadOperation
+  public List<String> getAvailableEndpoints() {
+    return List.of("/cars", "/trees");
+  }
+}
---
+++ sda-commons-app-example/src/test/java/org/sdase/commons/spring/boot/web/app/example/ActuatorEndpointTest.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2022- SDA SE Open Industry Solutions (https://www.sda.se)
+ *
+ * Use of this source code is governed by an MIT-style
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+package org.sdase.commons.spring.boot.web.app.example;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.sdase.commons.spring.boot.web.testing.auth.EnableSdaAuthMockInitializer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalManagementPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+@ContextConfiguration(initializers = {EnableSdaAuthMockInitializer.class})
+class ActuatorEndpointTest {
+
+  @LocalManagementPort private int managementPort;
+
+  @Autowired TestRestTemplate restTemplate;
+
+  @Test
+  void customActuatorEndpointShouldBeAvailable() {
+    var response =
+        restTemplate.getForEntity(
+            "http://localhost:" + managementPort + "/availableEndpoints", String.class);
+
+    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    assertThat(response.getBody()).contains("/cars", "/trees");
+  }
+}
